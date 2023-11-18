8f49130162347cf1e3c874bddb0f4822eff263c3
Gauravsaxenaking <151361833+Gauravsaxenaking@users.noreply.github.com>
2023-11-18 14:40:35 +0530

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,24 @@
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourHand = document.getElementById('hour-hand');
+    const minuteHand = document.getElementById('minute-hand');
+    const secondHand = document.getElementById('second-hand');
+
+    const hourDeg = (hours % 12) * 30 + minutes / 2;
+    const minuteDeg = minutes * 6 + seconds / 10;
+    const secondDeg = seconds * 6;
+
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+
+    setTimeout(updateClock, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateClock();
+});
